7ab3cb525350a29d158578aa13a609ee06dfd563
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-11-29 23:06:08 -0500

    Update EvaluateReversePolishNotation.cpp

--- EvaluateReversePolishNotation.cpp
+++ EvaluateReversePolishNotation.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        int pivot=1;
-        int i,j,k;
-        i=-1,j=-2;
-        for(k=0;k<n;k++)
-         if(A[k]<pivot){
-            if(j<i) swap(A[k],A[++i]),j++;
-            else{
-                swap(A[k],A[++j]);
-                swap(A[i++],A[j]);
+    void get_two_operands(int *a,int *b,stack<int> &st){
+        *a=st.top();
+        st.pop();
+        *b=st.top();
+        st.pop();
+    }
+    int evalRPN(vector<string> &tokens) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        stack<int> st;
+        int a,b;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+"){
+    
+                get_two_operands(&a,&b,st);
+                st.push(b+a);
             }
-         }else if(A[k]==pivot) {
-             if(j<i)j=i,i++;
-             swap(A[k],A[++j]);
-             
-         }
+            else if(tokens[i]=="-")
+            {
+                get_two_operands(&a,&b,st);
+                st.push(b-a);
+            }
+            else if(tokens[i]=="*"){
+                get_two_operands(&a,&b,st);
+                st.push(b*a);
+            }
+            else if(tokens[i]=="/"){
+                get_two_operands(&a,&b,st);
+                st.push(b/a);
+            }
+            else st.push(stoi(tokens[i]));
+        }
+        return st.top();
     }
 };
