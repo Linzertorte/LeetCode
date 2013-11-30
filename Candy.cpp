10f08c3326e4a73ce9ada1d0dd178e9ee80e1d29
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-11-29 23:08:51 -0500

    Update Candy.cpp

--- Candy.cpp
+++ Candy.cpp
@@ -1,20 +1,29 @@
+vector<int> rate;
+bool cmp(int i,int j){
+        return rate[i]<rate[j];
+}
+
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        int pivot=1;
-        int i,j,k;
-        i=-1,j=-2;
-        for(k=0;k<n;k++)
-         if(A[k]<pivot){
-            if(j<i) swap(A[k],A[++i]),j++;
-            else{
-                swap(A[k],A[++j]);
-                swap(A[i++],A[j]);
-            }
-         }else if(A[k]==pivot) {
-             if(j<i)j=i,i++;
-             swap(A[k],A[++j]);
-             
-         }
+    
+    
+    int candy(vector<int> &ratings) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        rate=ratings;
+        vector<int> idx(ratings.size());
+        for(int i=0;i<ratings.size();i++) idx[i]=i;
+        sort(idx.begin(),idx.end(),cmp);
+        int sum=0;
+        int n=ratings.size();
+        vector<int> candy(ratings.size(),0);
+        for(int i=0;i<ratings.size();i++){
+            int p=idx[i];
+            if(p>0&&ratings[p]>ratings[p-1]) candy[p]=max(candy[p],candy[p-1]+1);
+            if(p<n-1&&ratings[p]>ratings[p+1]) candy[p]=max(candy[p],candy[p+1]+1);
+            if(!candy[p]) candy[p]=1;
+        }
+        for(int i=0;i<candy.size();i++) sum+=candy[i];
+        return sum;
     }
 };
