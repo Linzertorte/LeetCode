fd609415d26aa7d18865fb2162ab0e4057f4e413
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-11-29 23:07:17 -0500

    Update BinaryTreePreorderTraversal.cpp

--- BinaryTreePreorderTraversal.cpp
+++ BinaryTreePreorderTraversal.cpp
@@ -1,20 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        int pivot=1;
-        int i,j,k;
-        i=-1,j=-2;
-        for(k=0;k<n;k++)
-         if(A[k]<pivot){
-            if(j<i) swap(A[k],A[++i]),j++;
-            else{
-                swap(A[k],A[++j]);
-                swap(A[i++],A[j]);
-            }
-         }else if(A[k]==pivot) {
-             if(j<i)j=i,i++;
-             swap(A[k],A[++j]);
-             
-         }
+    vector<int> preorderTraversal(TreeNode *root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        vector<int> ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        if (!root) return ans;
+        while(!st.empty()){
+            TreeNode* t = st.top();
+            ans.push_back(t->val),st.pop();
+     
+                 if(t->right) st.push(t->right);
+                 if(t->left) st.push(t->left);
+            
+            
+        }
+        return ans;
     }
 };
+
