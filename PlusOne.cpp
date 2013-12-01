aa5aa998740ecf3a98ff0d2efcf9a7d2c0e740b2
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-12-01 00:54:46 -0500

    Create PlusOne.cpp

---
+++ PlusOne.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        reverse(digits.begin(),digits.end());
+        int carry=1;
+        for(int i=0;i<digits.size();i++){
+            int d=digits[i];
+            digits[i]=(carry+d)%10;
+            carry=(carry+d)/10;
+        }
+        if(carry) digits.push_back(1);
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
