38c32df1b3880185dd3850d7e03ad054d441211b
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-12-18 20:38:00 -0500

    Create ContainerWithMostWater.cpp

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+     int low=0,high=height.size()-1;
+     int ans=0;
+     while(low<high){
+         ans=max(ans,(high-low)*min(height[low],height[high]));
+         if(height[low]<height[high]) low++;
+         else high--;
+         
+     }
+    return ans;
+    }
+};
