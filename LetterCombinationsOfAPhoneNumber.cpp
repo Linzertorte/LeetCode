46595702ca6b9479f7d484479b5aafad35e9ba2e
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-21 00:26:36 -0700

    Update LetterCombinationsOfAPhoneNumber.cpp

--- LetterCombinationsOfAPhoneNumber.cpp
+++ LetterCombinationsOfAPhoneNumber.cpp
@@ -1,25 +1,17 @@
 class Solution {
-     char table[10][5]={"xxx","xxx","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 public:
-    void dfs(const string &digits,string word,int cur,vector<string> &result){
-        if(cur==digits.size()) {
-            result.push_back(word);
-            return;
-        }
-        int d=digits[cur]-'0';
-        char letter;
-        string tmp;
-        for(int i=0;letter=table[d][i];i++){
-            tmp=word;
-            dfs(digits,tmp.append(1,letter),cur+1,result);
-        }
-    }
+    string tbl[10]={"xxx","xxx","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> letterCombinations(string digits) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-       vector<string> result;
-       
-       dfs(digits,"",0,result);
-       return result;
+        vector<string> result;
+        if(digits.size()==0) return result;
+        result.push_back("");
+        vector<string> cur;
+        for(auto c:digits){
+            cur = result;
+            result.clear();
+            for(auto d:tbl[c-'0'])
+                for(auto s:cur) result.push_back(s+d);
+        }
+        return result;
     }
 };
